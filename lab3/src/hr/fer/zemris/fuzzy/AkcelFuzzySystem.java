e9519d57396a1b33d831598c2ca67c999cd580ee
Matija Maric <sirSlani@gmail.com>
2017-11-12 01:12:10 +0100

    RADI AHHAHAHAHAHAH

--- lab3/src/hr/fer/zemris/fuzzy/AkcelFuzzySystem.java
+++ lab3/src/hr/fer/zemris/fuzzy/AkcelFuzzySystem.java
@@ -5,35 +5,24 @@ import java.util.ArrayList;
 public class AkcelFuzzySystem extends FuzzySystem {
 
     {
-        IDomena acceleration = Domain.intRange(-50, 50);
+        IDomena acceleration = Domain.intRange(-50, 51);
         IDomena distance = Domain.intRange(0, 1301);
-        IDomena velocity = Domain.intRange(0, 51);
+        IDomena velocity = Domain.intRange(0, 101);
         IDomena direction = Domain.intRange(0, 2);
 
         IFuzzySet idDistance = IdentityFuzzySet.get(distance);
-        IFuzzySet idVelocity = IdentityFuzzySet.get(velocity);
         IFuzzySet idDirection = IdentityFuzzySet.get(direction);
 
-        IFuzzySet ubrzaj = new MutableFuzzySet(acceleration).set(DomainElement.of(5), 1);
-        IFuzzySet uspori = new MutableFuzzySet(acceleration).set(DomainElement.of(-5), 1);
-        IFuzzySet nulaAcc = new MutableFuzzySet(acceleration).set(DomainElement.of(0), 1);
+        IFuzzySet ubrzaj = new CalculatedFuzzySet(velocity, StandardFuzzySets.lambdaFunction(52, 55, 58));
+        IFuzzySet uspori = new CalculatedFuzzySet(velocity, StandardFuzzySets.lFunction(40, 50));
 
-        IFuzzySet blizuZida = new CalculatedFuzzySet(distance, StandardFuzzySets.lFunction(50, 75));
-
-        IFuzzySet sporo = new CalculatedFuzzySet(velocity, StandardFuzzySets.lFunction(10, 20));
-        IFuzzySet brzo = new CalculatedFuzzySet(velocity, StandardFuzzySets.lambdaFunction(20,30, 40));
-        IFuzzySet prebrzo = new CalculatedFuzzySet(velocity, StandardFuzzySets.gammaFunction(30, 40));
-
-        IFuzzySet naprijed = new MutableFuzzySet(direction).set(DomainElement.of(1), 1);
-        IFuzzySet natrag = new MutableFuzzySet(direction).set(DomainElement.of(0), 1);
+        IFuzzySet sporo = new CalculatedFuzzySet(velocity, StandardFuzzySets.lFunction(20, 45));
+        IFuzzySet prebrzo = new CalculatedFuzzySet(velocity, StandardFuzzySets.gammaFunction(55, 70));
 
         // L, D, LK, RK, V, S
 
-        addRule(new IFuzzySet[] {idDistance,    idDistance,     idDistance,     idDistance,     idVelocity,     naprijed},  ubrzaj);
-        addRule(new IFuzzySet[] {idDistance,    idDistance,     idDistance,     idDistance,     idVelocity,     natrag},    uspori);
         addRule(new IFuzzySet[] {idDistance,    idDistance,     idDistance,     idDistance,     prebrzo,     idDirection},    uspori);
         addRule(new IFuzzySet[] {idDistance,    idDistance,     idDistance,     idDistance,     sporo,     idDirection},    ubrzaj);
-        addRule(new IFuzzySet[] {idDistance,    idDistance,     idDistance,     idDistance,     brzo,     idDirection},    nulaAcc);
 
     }
 
--- lab3/src/hr/fer/zemris/fuzzy/KormiloFuzzySystem.java
+++ lab3/src/hr/fer/zemris/fuzzy/KormiloFuzzySystem.java
@@ -5,30 +5,27 @@ public class KormiloFuzzySystem extends FuzzySystem {
     {
         IDomena angle = Domain.intRange(-90, 91);
         IDomena distance = Domain.intRange(0, 1301);
-        IDomena velocity = Domain.intRange(0, 51);
+        IDomena velocity = Domain.intRange(0, 101);
         IDomena direction = Domain.intRange(0, 2);
 
         IFuzzySet idDistance = IdentityFuzzySet.get(distance);
         IFuzzySet idVelocity = IdentityFuzzySet.get(velocity);
         IFuzzySet idDirection = IdentityFuzzySet.get(direction);
 
-        IFuzzySet blizuZida = new CalculatedFuzzySet(distance, StandardFuzzySets.lambdaFunction(75, 100, 125));
-        IFuzzySet jakoBlizuZida = new CalculatedFuzzySet(distance, StandardFuzzySets.lFunction(50, 100));
+        IFuzzySet blizuZida = new CalculatedFuzzySet(distance, StandardFuzzySets.lFunction(45, 65));
 
-        IFuzzySet skreniLijevo = new CalculatedFuzzySet(angle, StandardFuzzySets.lambdaFunction(0,15,30));
-        IFuzzySet skreniOstroLijevo = new CalculatedFuzzySet(angle, StandardFuzzySets.lambdaFunction(20,40,60));
-        IFuzzySet skreniDesno = new CalculatedFuzzySet(angle, StandardFuzzySets.lambdaFunction(-30, -15, 0));
-        IFuzzySet skreniOstroDesno = new CalculatedFuzzySet(angle, StandardFuzzySets.lambdaFunction(-60, -40, -20));
+        IFuzzySet skreniLijevo = new CalculatedFuzzySet(angle, StandardFuzzySets.gammaFunction(110, 130));
+        IFuzzySet skreniOstroLijevo = new CalculatedFuzzySet(angle, StandardFuzzySets.gammaFunction(150, 180));
+        IFuzzySet skreniDesno = new CalculatedFuzzySet(angle, StandardFuzzySets.lFunction(50, 70));
+        IFuzzySet skreniOstroDesno = new CalculatedFuzzySet(angle, StandardFuzzySets.lFunction(0, 30));
 
-        IFuzzySet naprijed = new MutableFuzzySet(direction).set(DomainElement.of(1), 1);
-        IFuzzySet natrag = new MutableFuzzySet(direction).set(DomainElement.of(0), 1);
+        IFuzzySet kriviSmjer = new MutableFuzzySet(direction).set(DomainElement.of(0), 1);
 
         // L, D, LK, RK, V, S
 
-        addRule(new IFuzzySet[] {idDistance,    blizuZida,      idDistance,     idDistance,     idVelocity,     idDirection},   skreniLijevo);
-        addRule(new IFuzzySet[] {idDistance,    jakoBlizuZida,  idDistance,     idDistance,     idVelocity,     idDirection},   skreniOstroLijevo);
-        addRule(new IFuzzySet[] {blizuZida,     idDistance,     idDistance,     idDistance,     idVelocity,     idDirection},   skreniDesno);
-        addRule(new IFuzzySet[] {jakoBlizuZida, idDistance,     idDistance,     idDistance,     idVelocity,     idDirection},   skreniOstroDesno);
+        addRule(new IFuzzySet[] {idDistance,    idDistance,      idDistance,     blizuZida,     idVelocity,     idDirection},   skreniOstroLijevo);
+        addRule(new IFuzzySet[] {idDistance,    idDistance,     blizuZida,     idDistance,     idVelocity,     idDirection},   skreniOstroDesno);
+        addRule(new IFuzzySet[] {idDistance,    idDistance,     idDistance,     idDistance,     idVelocity,     kriviSmjer},   skreniOstroLijevo);
     }
 
     public KormiloFuzzySystem(IDefuzzifier defuzzifier, IBinaryFunction tNorm, IBinaryFunction sNorm, IBinaryFunction implication) {
